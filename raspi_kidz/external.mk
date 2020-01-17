93b11217d9a8acb742a73e9c8ed1c50e234b678e
Juerg Haefliger <juergh@gmail.com>
2020-01-17 14:40:44 +0100

    Add qplayer package
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

---
+++ raspi_kidz/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_raspi_kidz_PATH)/package/*/*.mk))
---
+++ raspi_kidz/package/qplayer/qplayer.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# qplayer
+#
+################################################################################
+
+QPLAYER_VERSION = 0.1.0
+QPLAYER_SITE = $(TOPDIR)/../../../qplayer
+QPLAYER_SITE_METHOD = local
+QPLAYER_DEPENDENCIES = qt5base
+
+define QPLAYER_CONFIGURE_CMDS
+    cd $(@D) && $(TARGET_MAKE_ENV) qmake
+endef
+
+define QPLAYER_BUILD_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) all
+endef
+
+define QPLAYER_INSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install INSTALL_ROOT=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
