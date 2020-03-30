ace0df8ac1493eda4db8fe44711a3cb3363117c3
Juerg Haefliger <juergh@gmail.com>
2020-03-30 07:50:15 +0200

    post-image.sh: Enable audio
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

--- raspi_kidz/board/post-image.sh
+++ raspi_kidz/board/post-image.sh
@@ -6,6 +6,10 @@ cat << __EOF__ > "${BINARIES_DIR}"/rpi-firmware/cmdline.txt
 root=/dev/mmcblk0p2 rootwait console=ttyAMA0,115200 logo.nologo vt.global_cursor_default=0
 __EOF__
 
+# Enable audio and the serial console
+sed -i -e '/^# \[post-image\]$/,$d' "${BINARIES_DIR}"/rpi-firmware/config.txt
 cat << __EOF__ >> "${BINARIES_DIR}"/rpi-firmware/config.txt
+# [post-image]
+dtparam=audio=on
 enable_uart=1
 __EOF__
