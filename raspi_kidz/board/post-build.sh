d4ea370737560acf09baf16a80f6b54e71d85e11
Juerg Haefliger <juergh@gmail.com>
2020-03-30 07:57:23 +0200

    post-build.sh: Automatically log into the serial console as 'admin'
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

--- raspi_kidz/board/post-build.sh
+++ raspi_kidz/board/post-build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-set -u
 set -e
 
-# Remove the console on tty1
-if [ -e "${TARGET_DIR}"/etc/inittab ]; then
-	sed -i 's/^tty1::/#[post-build] tty1::/' "${TARGET_DIR}"/etc/inittab
-fi
+# Remove all console and tty1 lines
+sed -i -e '/^console::/d' -e '/^tty1::/d' "${TARGET_DIR}"/etc/inittab
+
+# Remove everything after '# [post-build]'
+sed -i -e '/^# \[post-build\]$/,$d' "${TARGET_DIR}"/etc/inittab
+
+# Add getty/login to the serial and HDMI consoles
+cat << __EOF__ >> "${TARGET_DIR}"/etc/inittab
+# [post-build]
+# tty1::respawn:/sbin/getty -L tty1 0 vt100 # HDMI console
+console::respawn:/bin/login -f admin
+__EOF__
