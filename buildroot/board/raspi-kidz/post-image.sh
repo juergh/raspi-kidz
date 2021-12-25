6ec242509d2444c5d2b4206b0badf2e635a050e4
Juerg Haefliger <juergh@gmail.com>
2021-12-25 19:56:01 +0100

    raspi-kidz: Use buildroot's genimage.sh script
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

--- buildroot/board/raspi-kidz/post-image.sh
+++ buildroot/board/raspi-kidz/post-image.sh
@@ -48,30 +48,3 @@ dtparam=pwr_led_activelow=off
 dtparam=act_led_trigger=none
 dtparam=act_led_activelow=off
 __EOF__
-
-#
-# Generate the image (copied from buildroot/board/raspberrypi3/post-image.sh)
-#
-
-BOARD_DIR="$(dirname "$0")"
-GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
-
-# Pass an empty rootpath. genimage makes a full copy of the given rootpath to
-# ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
-# space. We don't rely on genimage to build the rootfs image, just to insert a
-# pre-built one in the disk image.
-
-trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
-ROOTPATH_TMP="$(mktemp -d)"
-
-rm -rf "${GENIMAGE_TMP}"
-
-genimage \
-	--rootpath "${ROOTPATH_TMP}"   \
-	--tmppath "${GENIMAGE_TMP}"    \
-	--inputpath "${BINARIES_DIR}"  \
-	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
-
-exit $?
