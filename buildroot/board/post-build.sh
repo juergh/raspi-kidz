427110d5492a2f02e0f5d6ce364ce3bb9cdeddfc
Juerg Haefliger <juergh@gmail.com>
2021-12-25 19:43:27 +0100

    Move the generation of the updater initrd to the post-build.sh script
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

--- buildroot/board/post-build.sh
+++ buildroot/board/post-build.sh
@@ -35,6 +35,35 @@ fi
 ssh-keygen -A -f "${TARGET_DIR}"
 
 #
+# Create the updater initrd
+#
+
+initrd_dir="${BINARIES_DIR}"/initrd.d
+rm -rf "${initrd_dir}"
+mkdir "${initrd_dir}"
+
+# Copy the init script
+cp "${BR2_EXTERNAL_RASPI_KIDZ_PATH}"/board/init.sh  "${initrd_dir}"/init
+
+# Install busybox
+mkdir -p "${initrd_dir}"/bin
+cp "${TARGET_DIR}"/bin/busybox "${initrd_dir}"/bin
+
+# Install libc and ld
+mkdir -p "${initrd_dir}"/lib
+cp -dp "${TARGET_DIR}"/lib/libc.* "${TARGET_DIR}"/lib/libuClibc-* "${TARGET_DIR}"/lib/ld* "${initrd_dir}"/lib
+ln -s lib "${initrd_dir}"/lib32
+
+# Create the initrd image
+( cd "${initrd_dir}" && \
+  find . | cpio -H newc -o | gzip -9 > "${BINARIES_DIR}"/initrd.img )
+
+# Copy the initrd if necessary
+if [ -e "${TARGET_DIR}"/boot/bzImage ] ; then
+	cp "${BINARIES_DIR}"/initrd.img "${TARGET_DIR}"/boot
+fi
+
+#
 # Fix grub
 #
 
